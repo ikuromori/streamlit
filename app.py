fb416a8eb574f80ecca083322dcd5a60a0bd8ee9
IKURO MORI <196.0507.im@gmail.com>
2023-08-15 09:49:20 +0900

    ３つコミット

--- app.py
+++ app.py
@@ -3,3 +3,10 @@ import streamlit as st
 
 st.title('Simple Streamlit App')
 st.write('Welcome to my app!')
+
+# テキストボックスを追加
+user_input = st.text_input("あなたの名前を入力してください:", "")
+
+# テキストボックスの内容を表示
+if user_input:
+    st.write(f"こんにちは、{user_input}さん！")
